43882d6ec10b135a38f5325a4818f6adb50bcc66
Pavel Svitov <95366264+psvitov@users.noreply.github.com>
2022-09-14 20:28:22 +0400

    Update WelcomerTest.java

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -18,16 +18,11 @@ public class WelcomerTest {
 		assertThat(welcomer.sayFarewell(), containsString("Farewell"));
 	}
 	@Test
-	public void welcomerSaysHunter() {
-		assertThat(welcomer.sayWelcome(), containsString("hunter"));
-		assertThat(welcomer.sayFarewell(), containsString("hunter"));
-	}
-	@Test
 	public void welcomerSaysSilver(){
 		assertThat(welcomer.sayNeedGold(), containsString("gold"));
 	}
 	@Test
-	public void welcomerSaysSomething(){
-		assertThat(welcomer.saySome(), containsString("something"));
+	public void welcomerSaysHunter() {
+		assertThat(welcomer.sayHunter(), containsString("hunter"));
 	}
 }
